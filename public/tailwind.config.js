f6329e63f40dcb91725051d727f9808144dadd01
irradev <israeldevcorel@gmail.com>
2023-10-06 17:15:42 -0600

    init

---
+++ public/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['./**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     fontFamily: {
+//       dancing: ['"Dancing Script"', 'cursive'],
+//     },
+//   },
+// };
